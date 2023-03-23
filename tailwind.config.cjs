c589d8a432c220d77532d840d283f89f5c7970e9
Erick de Bem Campos <erickcamposbem69@gmail.com>
2023-03-23 14:04:12 -0300

    added fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      Quintessential: ["Quintessential", "sans"],
+    },
+    colors: {
+      bgForm: "#EFF0EE",
+      bgInput: "#FDFEFE",
+      bgButtonLogin: "#F4A82B",
+      bgIconLogin: "#C04933",
+    },
+    extend: {
+      backgroundImage: {
+        Sunflower: "url('./src/assets/SunflowerBg.png')",
+      },
+    },
   },
   plugins: [],
 };
