e9e132239c79b32a7f4e6ffa45e1683f563503b6
Erick de Bem Campos <erickcamposbem69@gmail.com>
2023-04-03 13:27:40 -0300

    rotas e rotas dinamicas ajustadas

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,16 +1,16 @@
 import { Route, Routes } from "react-router-dom";
+import { Grupos } from "./pages/Grupos";
 import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
-import { Mensagens } from "./pages/Mensagens";
-import { TextMensage } from "./pages/TextMensage";
+import { Grupo } from "./pages/Grupo";
 
 const Rotas = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/home" element={<Home />} />
-      <Route path="/home/mensagens" element={<Mensagens />} />
-      <Route path="/home/mensagens/:id" element={<TextMensage />} />
+      <Route path="/home/grupos" element={<Grupos />} />
+      <Route path="/home/grupos/:id" element={<Grupo />} />
     </Routes>
   );
 };
