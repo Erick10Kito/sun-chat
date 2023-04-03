50f542abf535f66c6fbee80732f9fc45bd20161d
Erick de Bem Campos <erickcamposbem69@gmail.com>
2023-04-03 13:28:06 -0300

    adicionado mais cores e mais backgrounds

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,10 +11,14 @@ module.exports = {
       bgInput: "#FDFEFE",
       bgButtonLogin: "#F4A82B",
       bgIconLogin: "#C04933",
+      white: "#fff",
+      yellow1: "#F9CD58",
+      gray1: "#b8b8b8",
     },
     extend: {
       backgroundImage: {
         Sunflower: "url('./src/assets/SunflowerBg.png')",
+        Sunshine: "url('./src/assets/SunshineBg.png')",
       },
     },
   },
