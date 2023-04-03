90bc6f026f1675543a50613c64ca458d76578837
Erick de Bem Campos <erickcamposbem69@gmail.com>
2023-04-03 13:29:42 -0300

    Componente Header estilizado e funcional

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,8 @@
+export function Header() {
+  return (
+    <div className="absolute top-0 left-0 h-16 bg-[transparent] px-7 w-full flex items-center justify-between bg-opacity-50">
+      <h1 className="text-5xl">LOGO</h1>
+      <button>Sair</button>
+    </div>
+  );
+}
