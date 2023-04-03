f7758d87db880a0975076a688d22c62827797158
Erick de Bem Campos <erickcamposbem69@gmail.com>
2023-04-03 13:31:14 -0300

    Lista de dados atualizada

--- src/data/api.ts
+++ src/data/api.ts
@@ -1,16 +1,86 @@
-export const ApiMensagens = [
+export const Dados = [
   {
-    id: 1,
-    title: "Teste",
-    text: "to testando caralho",
-    by: "Erick",
-    to: "Teste",
+    grupoId: "1",
+    grupoName: "Nome do Grupo",
+    grupoAuthor: "Erick",
+    createdDate: new Date(),
+    members: [
+      {
+        idMember: "5",
+        nameMember: "Teste",
+      },
+    ],
+    mensagens: [
+      {
+        mensagemId: "2",
+        mensagemTitle: "Titulo da Mensagem",
+        mensagemContent: "Meu conteudo",
+        mensagemAuthor: "Erick",
+        sendDate: new Date(),
+      },
+      {
+        mensagemId: "3",
+        mensagemTitle: "Titulo da Mensagem",
+        mensagemContent: "Meu conteudo",
+        mensagemAuthor: "Erick",
+        sendDate: new Date(),
+      },
+      {
+        mensagemId: "4",
+        mensagemTitle: "Titulo da Mensagem",
+        mensagemContent: "Meu conteudo",
+        mensagemAuthor: "Erick",
+        sendDate: new Date(),
+      },
+      {
+        mensagemId: "5",
+        mensagemTitle: "Titulo da Mensagem",
+        mensagemContent: "Meu conteudo",
+        mensagemAuthor: "Erick",
+        sendDate: new Date(),
+      },
+    ],
   },
   {
-    id: 2,
-    title: "tete",
-    text: "to tetando caralho",
-    by: "tete",
-    to: "teter",
+    grupoId: "54",
+    grupoName: "Nome do Grupo",
+    grupoAuthor: "Erick",
+    createdDate: new Date(),
+    members: [
+      {
+        idMember: "54562458555",
+        nameMember: "Teste",
+      },
+    ],
+    mensagens: [
+      {
+        mensagemId: "45456",
+        mensagemTitle: "Titulo da Mensagem",
+        mensagemContent: "Meu conteudo",
+        mensagemAuthor: "Erick",
+        sendDate: new Date(),
+      },
+      {
+        mensagemId: "65454",
+        mensagemTitle: "Titulo da Mensagem",
+        mensagemContent: "Meu conteudo",
+        mensagemAuthor: "Erick",
+        sendDate: new Date(),
+      },
+      {
+        mensagemId: "548742",
+        mensagemTitle: "Titulo da Mensagem",
+        mensagemContent: "Meu conteudo",
+        mensagemAuthor: "Erick",
+        sendDate: new Date(),
+      },
+      {
+        mensagemId: "56656",
+        mensagemTitle: "Titulo da Mensagem",
+        mensagemContent: "Meu contdasdasfudo",
+        mensagemAuthor: "Erick",
+        sendDate: new Date(),
+      },
+    ],
   },
 ];
