6bce293d63df973e3e68f720f5bb802fa8f2e60a
Erick de Bem Campos <erickcamposbem69@gmail.com>
2023-04-05 13:10:55 -0300

    Logo Adcionada, botões de voltar e sair adicionados

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,17 @@
+import { Link } from "react-router-dom";
+import Logo from "../../assets/logoBgWhite.png";
+
 export function Header() {
   return (
-    <div className="absolute top-0 left-0 h-16 bg-[transparent] px-7 w-full flex items-center justify-between bg-opacity-50">
-      <h1 className="text-5xl">LOGO</h1>
-      <button>Sair</button>
+    <div className="absolute top-0 left-0 bg-[transparent] px-7 w-full flex items-center justify-between bg-opacity-50 py-5">
+      <img src={Logo} alt="" className="w-[120px]" />
+
+      <Link
+        to="/"
+        className="py-1 px-6 bg-yellow1 rounded shadow hover:bg-[#f9ce58a9]"
+      >
+        Sair
+      </Link>
     </div>
   );
 }
--- src/components/SidebarGroups/index.tsx
+++ src/components/SidebarGroups/index.tsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import { GroupsButton } from "../GroupsButton";
+import Logo from "../../assets/logoBgWhite.png";
+import { Link } from "react-router-dom";
+import { ArrowElbowDownLeft } from "phosphor-react";
 
 export function SidebarGroups({ slug }: any) {
   const [showGroup, setShowGroup] = useState(false);
@@ -8,14 +11,24 @@ export function SidebarGroups({ slug }: any) {
     setShowGroup(!showGroup);
   }
   return (
-    <div className="w-[300px] bg-white shadow-2xl h-screen py-5">
-      <div className="flex justify-center pb-5 ">
-        <h1>LOGO</h1>
-      </div>
-      <div className="">
-        <div>
-          <GroupsButton slug={slug} />
+    <div className="w-[300px] bg-white shadow-2xl h-screen py-5 flex flex-col">
+      <div>
+        <div className="flex justify-center pb-5 ">
+          <Link to="/home">
+            <img src={Logo} alt="" className="w-[150px]" />
+          </Link>
         </div>
+        <div className="">
+          <div>
+            <GroupsButton slug={slug} />
+          </div>
+        </div>
+      </div>
+      <div className="flex-1 flex items-end px-4">
+        <Link to="/home" className="flex items-center gap-1">
+          <ArrowElbowDownLeft size={24} weight="bold" />
+          <p>Voltar</p>
+        </Link>
       </div>
     </div>
   );
